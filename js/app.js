60a9828bfe3e1686c475df22775ac7fe9f7a8e80
cv <34649812+cpvalente@users.noreply.github.com>
2021-01-30 14:55:14 +0100

    lint

--- js/app.js
+++ js/app.js
@@ -11,8 +11,7 @@
  *
  * JS Standard: ESlint
  *
-*/
-
+ */
 
 // better scroll event handling
 // https://www.javascripttutorial.net/javascript-dom/javascript-scroll-events/
@@ -20,17 +19,17 @@
 /**
  * Define Global Variables
  *
-*/
+ */
 const titleSelect = 'h2.landing__title';
 const sectionSelect = 'section';
 const navListSelect = 'navbar__list';
 const navBarSelect = '.navbar__menu';
 const activeCSS = 'is__active';
 const navLinkSelect = 'menu__link';
-const navPrefix = 'nav-'
-const fadeDownCSS = 'opacityDown'
+const navPrefix = 'nav-';
+const fadeDownCSS = 'opacityDown';
 const intersectThresh = 0.75;
-const NavToTop = 'navToTop'
+const NavToTop = 'navToTop';
 
 // get all sections
 const sections = document.querySelectorAll(sectionSelect);
@@ -44,32 +43,30 @@ const navList = document.getElementById(navListSelect);
 // intersection API
 let options = {
   threshold: intersectThresh,
-}
+};
 
 let observer = new IntersectionObserver(observerHandler, options);
-
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
-*/
-
+ */
 
 /**
-* @description Sets opacity of nav element down
-* @param none
-* @returns none
-*/
+ * @description Sets opacity of nav element down
+ * @param none
+ * @returns none
+ */
 let setNavbarOpacityDown = () => {
   document.querySelector(navBarSelect).classList.add(fadeDownCSS);
-}
+};
 
 /**
-* @description When user scrolls, fade up navbar and add navigate to top
-* @param none
-* @returns none
-*/
+ * @description When user scrolls, fade up navbar and add navigate to top
+ * @param none
+ * @returns none
+ */
 let onScroll = () => {
   document.querySelector(navBarSelect).classList.remove(fadeDownCSS);
 
@@ -81,33 +78,30 @@ let onScroll = () => {
 
   // call clearScroll with timeout
   setTimeout(setNavbarOpacityDown, 10000);
-}
+};
 
 /**
-* @description Toggles navigate to top element
-* @param {boolean} toggle
-* @returns none
-*/
+ * @description Toggles navigate to top element
+ * @param {boolean} toggle
+ * @returns none
+ */
 let activateScrollToTopEl = (toggle) => {
   const el = document.getElementById(NavToTop);
-  toggle
-    ?
-    el.classList.add(activeCSS)
-    :
-    el.classList.remove(activeCSS);
-}
+  toggle ? el.classList.add(activeCSS) : el.classList.remove(activeCSS);
+};
 
 /**
-* @description Sets window to given Y position
-* @param {number} posY
-* @returns none
-*/
+ * @description Sets window to given Y position
+ * @param {number} posY
+ * @returns none
+ */
 let navigateTo = (posY) => {
   window.scrollTo({
     top: posY,
-    behavior: 'smooth'
+    behavior: 'smooth',
   });
-}
+};
+
 /**
  * @description Sets window to given element position
  * @param {HTMLElement} element
@@ -143,29 +137,27 @@ let clickEventHandler = (event) => {
 };
 
 /**
-* @description Clears active class from all navigation elements
-* @param none
-* @returns none
-*/
+ * @description Clears active class from all navigation elements
+ * @param none
+ * @returns none
+ */
 let clearActiveNav = () => {
   const navEls = document.getElementsByClassName(navLinkSelect);
   for (e of navEls) {
     e.classList.remove(activeCSS);
-  };
-}
+  }
+};
 
 /**
-* @description Observer handler function, adds active class to intersecting object
-* @param {HTMLAllCollection} entries
-* @returns none
-*/
+ * @description Observer handler function, adds active class to intersecting object
+ * @param {HTMLAllCollection} entries
+ * @returns none
+ */
 function observerHandler(entries) {
-
   // clear active class
   clearActiveNav();
 
   for (entry of entries) {
-
     if (entry.isIntersecting) {
       let t = entry.target;
       // add active CSS to section
@@ -184,20 +176,19 @@ function observerHandler(entries) {
  * End Helper Functions
  * Begin Main Functions
  *
-*/
+ */
 
 /**
-* @description Build navigation bar
-* @param none
-* @returns none
-*/
+ * @description Build navigation bar
+ * @param none
+ * @returns none
+ */
 let buildNav = () => {
   // create utility framgment
   const fragment = document.createDocumentFragment();
 
   // for every title we append an in nav
   for (let i = 0; i < titles.length; i++) {
-
     // create a list element
     const item = document.createElement('li');
 
@@ -223,16 +214,17 @@ let buildNav = () => {
   }
 
   navList.appendChild(fragment);
-}
+};
 
 /**
  * End Main Functions
  * Begin Events
  *
-*/
+ */
 
 // add listener to scroller events
-document.addEventListener('scroll', onScroll)
+document.addEventListener('scroll', onScroll);
+
 // add event listener to clicks on navbar
 navList.addEventListener('click', clickEventHandler);
 
